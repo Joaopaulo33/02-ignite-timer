9a61609ab02410cca9f0e7fd4fb0cd21afb26d92
joaopaulo33 <joaopaulo33573@gmail.com>
2022-09-13 20:30:03 -0300

    defautlayout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefaultLayout/index'
 import { History } from './pages/History'
 import { Home } from './pages/Home'
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
